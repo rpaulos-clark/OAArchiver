5d688e1d181d67eb5a439943855e834a5656c4ec
Ryan <rpaulos@clark.edu>
2018-08-21 10:57:31 -0700

    +comment for bug hunt

--- mainMain.py
+++ mainMain.py
@@ -2,7 +2,8 @@ import RetrieveProgramData as rpd
 import ui
 
 
-""" BUG: Outcomes are sometimes switched between AAS and CP accounting. Weird
+""" BUG: Outcomes are sometimes switched between AAS and CP accounting. Build program groups uses a dictionary, so
+    that is a likely candidate. Upgrade to 3.7 and that will probably resolve itself
 """
 
 """ Should the user be allowed to pick a year/quarter assessed? Probably, otherwise we rely upon timely filing LOL
